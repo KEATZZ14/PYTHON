1ca036f3e563c543d4424a5dc260c95f6e5b6ccb
KEATZZ14 <36299342+KEATZZ14@users.noreply.github.com>
2018-05-24 16:06:03 +0530

    Create 57.py

---
+++ 57.py
@@ -0,0 +1,5 @@
+a = int(input("enter the first num:"))
+b = int(input("enter the second num:"))
+print("\nbefore swap a = %d and b = %d" %(a,b))
+a, b = b, a
+print("\nafter swap a = %d and b = %d" %(a,b))
