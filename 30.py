6a83ecf07973633fa9c532c8383e26bbdfa47d36
KEATZZ14 <36299342+KEATZZ14@users.noreply.github.com>
2018-05-24 12:19:28 +0530

    Create 30.py

---
+++ 30.py
@@ -0,0 +1,6 @@
+a=int(input("enter the value in hr:"))
+b=int(input("enter the value in min:"))
+c=int(input("enter the value in hours:"))
+d=int(input("ente the value in minutes:"))
+if(b>60, d>60):
+    print(a-c, b-d)
